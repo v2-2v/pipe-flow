bcea97505f3b4df6113a354f4b07fb154da21e55
v2-2v <github@yoimiya.net>
2026-05-03 07:36:35 +0000

    add build tools

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "Building command-send.go..."
+go build -o ./build/ command-send.go
+
+echo "Building data-bank.go..."
+go build -o ./build/ data-bank.go
+
+echo "Building data-print.go..."
+go build -o ./build/ data-print.go
+
+echo "Building data-send.go..."
+go build -o ./build/ data-send.go
